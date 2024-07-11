5d0e1a54ed7fcad389015f7bdb5ab147f091d454
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2024-07-10 20:22:21 -0400

    update

--- 30-days-of-code/day-2-operators.js
+++ 30-days-of-code/day-2-operators.js
@@ -1,40 +1,14 @@
-// 'use strict';
-
-// process.stdin.resume();
-// process.stdin.setEncoding('utf-8');
-
-// let inputString = '';
-// let currentLine = 0;
-
-// process.stdin.on('data', function(inputStdin) {
-//     inputString += inputStdin;
-// });
-
-// process.stdin.on('end', function() {
-//     inputString = inputString.split('\n');
-
-//     main();
-// });
-
-// function readLine() {
-//     return inputString[currentLine++];
-// }
-
-// /*
-//  * Complete the 'solve' function below.
-//  *
-//  * The function accepts following parameters:
-//  *  1. DOUBLE meal_cost
-//  *  2. INTEGER tip_percent
-//  *  3. INTEGER tax_percent
-//  */
-
-// function solve(meal_cost, tip_percent, tax_percent) {
-//     // Write your code here
-
-// }
+/*
+  * Complete the 'solve' function below.
+  *
+  * The function accepts following parameters:
+  *  1. DOUBLE meal_cost
+  *  2. INTEGER tip_percent
+  *  3. INTEGER tax_percent
+  */
 
 function solve(meal_cost, tip_percent, tax_percent) {
+  // Write your code here
   let tip = (meal_cost / 100) * tip_percent;
   let tax = (tax_percent / 100) * meal_cost;
   let total_cost = meal_cost + tip + tax;
--- 30-days-of-code/day-3-intro-to-conditional-statements.js
+++ 30-days-of-code/day-3-intro-to-conditional-statements.js
@@ -1,31 +1,3 @@
-// 'use strict';
-
-// process.stdin.resume();
-// process.stdin.setEncoding('utf-8');
-
-// let inputString = '';
-// let currentLine = 0;
-
-// process.stdin.on('data', function(inputStdin) {
-//     inputString += inputStdin;
-// });
-
-// process.stdin.on('end', function() {
-//     inputString = inputString.split('\n');
-
-//     main();
-// });
-
-// function readLine() {
-//     return inputString[currentLine++];
-// }
-
-
-
-// function main() {
-//     const N = parseInt(readLine().trim(), 10);
-// }
-
 function main() {
   const N = parseInt(readLine(), 10);
   if (N % 2 != 0) {
