56bf9cf2d020599f706f598cd8c404db8c356061
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2024-07-13 18:35:03 -0400

    Create day-7-arrays.js

---
+++ 30-days-of-code/day-7-arrays.js
@@ -0,0 +1,9 @@
+function main() {
+  const n = parseInt(readLine(), 10);
+
+  const arr = readLine()
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  console.log(arr.reverse().join(" "));
+}
