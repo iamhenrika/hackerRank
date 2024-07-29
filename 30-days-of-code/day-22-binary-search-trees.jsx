5e37a99d1672802e5b039c29dfd9d09edab3a4a1
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2024-07-28 23:08:36 -0400

    Update day-22-binary-search-trees.jsx

---
+++ 30-days-of-code/day-22-binary-search-trees.jsx
@@ -0,0 +1,10 @@
+this.getHeight = function (root) {
+  // Add your code here
+  if (!root) {
+    return -1;
+  } else {
+    let l = this.getHeight(root.left);
+    let r = this.getHeight(root.right);
+    return (l > r ? l : r) + 1;
+  }
+}; // End of function getHeight
